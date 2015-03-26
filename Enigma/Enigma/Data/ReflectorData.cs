69234f7ef1cdcd5d6e098ced0d15738161497d5f
It423 <it42@hotmail.co.uk>
2015-03-26 20:05:58 +0000

    Add data representations of the rotor and reflector class

---
+++ Enigma/Enigma/Data/ReflectorData.cs
@@ -0,0 +1,37 @@
+﻿// ReflectorData.cs
+// <copyright file="ReflectorData.cs"> This code is protected under the MIT License. </copyright>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enigma.Data
+{
+    /// <summary>
+    /// A class that holds data about reflectors to be used.
+    /// </summary>
+    public class ReflectorData
+    {        
+        /// <summary>
+        /// Initailizes a new instance of the <see cref="ReflectorData" /> class.
+        /// </summary>
+        /// <param name="name"> The name of the reflector. </param>
+        /// <param name="wiring"> The wiring of the reflector. </param>
+        public ReflectorData(string name, string wiring)
+        {
+            this.Name = name;
+            this.Wiring = wiring;
+        }
+
+        /// <summary>
+        /// Gets or sets the name of the reflector.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the wiring to be used for this reflector.
+        /// </summary>
+        public string Wiring { get; set; }
+    }
+}
---
+++ Enigma/Enigma/Data/RotorData.cs
@@ -0,0 +1,44 @@
+﻿// RotorData.cs
+// <copyright file="RotorData.cs"> This code is protected under the MIT License. </copyright>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enigma.Data
+{
+    /// <summary>
+    /// A class that holds data about rotors to be used.
+    /// </summary>
+    public class RotorData
+    {
+        /// <summary>
+        /// Initailizes a new instance of the <see cref="RotorData" /> class.
+        /// </summary>
+        /// <param name="name"> The name of the rotor. </param>
+        /// <param name="wiring"> The wiring of the rotor. </param>
+        /// <param name="turningNotches"> The letters that will turn the next rotor. </param>
+        public RotorData(string name, string wiring, string turningNotches)
+        {
+            this.Name = name;
+            this.Wiring = wiring;
+            this.TunringNotches = turningNotches;
+        }
+
+        /// <summary>
+        /// Gets or sets the name of the rotor.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the wiring to be used for this rotor.
+        /// </summary>
+        public string Wiring { get; set; }
+
+        /// <summary>
+        /// Gets or sets the letter that will turn the rotor.
+        /// </summary>
+        public string TunringNotches { get; set; }
+    }
+}
