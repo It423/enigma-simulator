fceea98bf430263d7d67d252c2b113355a84c9fb
It423 <it42@hotmail.co.uk>
2015-03-26 19:56:28 +0000

    Change from mainwindow to machine setup

--- Enigma/Enigma/App.xaml.cs
+++ Enigma/Enigma/App.xaml.cs
@@ -1,11 +1,5 @@
 ﻿// App.xaml.cs
 // <copyright file="App.xaml.cs"> This code is protected under the MIT License. </copyright>
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace Enigma
