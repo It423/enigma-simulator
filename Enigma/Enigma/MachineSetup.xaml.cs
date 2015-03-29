4e77bec0d67f02e14eac63bd2f4bcf98f12a054f
It423 <it42@hotmail.co.uk>
2015-03-29 10:08:54 +0100

    Add code to switch display from rotor settings to ring settings

--- Enigma/Enigma/MachineSetup.xaml.cs
+++ Enigma/Enigma/MachineSetup.xaml.cs
@@ -31,7 +31,21 @@ namespace Enigma
         public MachineSetup()
         {
             this.InitializeComponent();
+
+            // Tell the program the default data
+            this.FourRotors = false;
+            this.RingSettingsActive = false;
         }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the ring settings are currently being viewed.
+        /// </summary>
+        public bool RingSettingsActive { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether there are four rotors or not.
+        /// </summary>
+        public bool FourRotors { get; set; }
 
         /// <summary>
         /// Begins encryption using the current enigma settings.
@@ -49,6 +63,54 @@ namespace Enigma
             // Show encryptor, hide settings
             ////ew.Show();
             this.Close();
+        }
+
+        /// <summary>
+        /// Changes which rotor setup is being viewed.
+        /// </summary>
+        /// <param name="sender"> The origin of the event. </param>
+        /// <param name="e"> The event arguments. </param>
+        private void SwitchRotorView_Click(object sender, RoutedEventArgs e)
+        {
+            // Get how many rotors to display
+            int rotorsToSwitch = this.FourRotors ? 4 : 3;
+
+            if (this.RingSettingsActive)
+            {
+                // Switch from ring settings to rotor positions
+                for (int i = 0; i < 8; i++)
+                {
+                    if (i < 4 || i >= 4 + rotorsToSwitch)
+                    {
+                        this.RotorSettingsGrid.ColumnDefinitions[i].Width = new GridLength(0, GridUnitType.Star);
+                    }
+                    else
+                    {
+                        this.RotorSettingsGrid.ColumnDefinitions[i].Width = new GridLength(1, GridUnitType.Star);
+                    }
+                }
+
+                // Tell the program it has switched
+                this.RingSettingsActive = !this.RingSettingsActive;
+            }
+            else
+            {
+                // Switch from rotor positions to ring settings
+                for (int i = 0; i < 8; i++)
+                {
+                    if (i < rotorsToSwitch)
+                    {
+                        this.RotorSettingsGrid.ColumnDefinitions[i].Width = new GridLength(1, GridUnitType.Star);
+                    }
+                    else
+                    {
+                        this.RotorSettingsGrid.ColumnDefinitions[i].Width = new GridLength(0, GridUnitType.Star);
+                    }
+                }
+
+                // Tell the program it has switched
+                this.RingSettingsActive = !this.RingSettingsActive;
+            }
         }
     }
 }
