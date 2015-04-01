dd40f09b716ad1141d29fe70f0b06c3074dc2d9c
It423 <it42@hotmail.co.uk>
2015-04-01 22:31:08 +0100

    Move the reflector/rotor data Was in enigma project, should be in utilities project

---
+++ Enigma/EnigmaUtilities/Data/ReflectorData.cs
@@ -0,0 +1,31 @@
+﻿// ReflectorData.cs
+// <copyright file="ReflectorData.cs"> This code is protected under the MIT License. </copyright>
+namespace EnigmaUtilities.Data
+{
+    /// <summary>
+    /// A class that holds data about reflectors to be used.
+    /// </summary>
+    public class ReflectorData
+    {        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReflectorData" /> class.
+        /// </summary>
+        /// <param name="name"> The name of the reflector. </param>
+        /// <param name="wiring"> The wiring of the reflector. </param>
+        public ReflectorData(string name, string wiring)
+        {
+            this.Name = name;
+            this.Wiring = wiring;
+        }
+
+        /// <summary>
+        /// Gets or sets the name of the reflector.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the wiring to be used for this reflector.
+        /// </summary>
+        public string Wiring { get; set; }
+    }
+}
