1986849f7b00cd3c3f0a587f72363cab1d14412a
It423 <it42@hotmail.co.uk>
2015-04-08 19:10:14 +0100

    Add create compoennt page

---
+++ Enigma/Enigma/CreateComponent.xaml.cs
@@ -0,0 +1,20 @@
+﻿// CreateComponent.xaml.cs
+// <copyright file="CreateComponent.xaml.cs"> This code is protected under the MIT License. </copyright>using System;
+using System.Windows;
+
+namespace Enigma
+{
+    /// <summary>
+    /// Interaction logic for CreateComponent.xaml
+    /// </summary>
+    public partial class CreateComponent : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CreateComponent" /> class.
+        /// </summary>
+        public CreateComponent()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
